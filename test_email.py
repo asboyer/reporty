77b81b18b1331d6480193b6a59144060c5b17a5c
Andrew Boyer <asboyer@gmail.com>
2020-07-28 15:49:43 -0400

    first commmit

---
+++ test_email.py
@@ -0,0 +1,15 @@
+import smtplib
+
+sender_email = input("Please enter your email: ")
+password = input("Please enter your password : ")
+rec_email = input("Please enter reciever email: ")
+
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, password)
+print("Login success")
+
+message = input("Enter your message to the reciever")
+server.sendmail(sender_email, rec_email, message)
+print("Email has been sent to ", rec_email)
