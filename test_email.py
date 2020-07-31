8050f1aa2bdc586d76187f7546fc10a8d946ba14
Benjamin Tengelsen <btengelsen@intelycare.com>
2020-07-31 15:33:47 -0400

    made things into functions

--- test_email.py
+++ test_email.py
@@ -1,15 +1,35 @@
 import smtplib
-from email_credentials import password, sender_email, rec_email
+from email_credentials import password, sender_email
 
-# sender_email = input("Please enter your email: ")
-# password = input("Please enter your password : ")
-# rec_email = input("Please enter reciever email: ")
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, password)
-print("Login success")
+def connect_email(sender_email, password):
+    """ sets up a smtp server
 
-message = input("Enter your message to the reciever")
-server.sendmail(sender_email, rec_email, message)
-print("Email has been sent to ", rec_email)
+    Args:
+        sender_email (str): senders email address
+        password (str): password for sender's email 
+
+    Returns:
+        smtplib.SMTP object
+    """
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, password)
+    print("Login success")
+
+    return server
+
+
+def send_email(server, receiver_email, message):
+    """ Sends email
+
+    """
+    server.sendmail(sender_email, rec_email, message)
+    print("Email has been sent to ", rec_email)
+
+
+if __name__ == "__main__":
+    connect_email(sender_email, password)
+    receiver_email = 'b.tengelsen@gmail.com'
+    message = 'hello world'
+    
