ee6346cd1a0203c98a5e06d1c201e13dd506371b
Benjamin Tengelsen <btengelsen@intelycare.com>
2020-08-04 15:00:27 -0400

    adding new file for making figures

---
+++ test_figures.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+
+
+def make_random_figure():
+    """ Makes a random figure (using pandas)
+
+    Returns a matplotlib figure
+    """
+    pass
+
+    # make some fake data or use numpy to get random data
+    # or get some real data from here https://pandas-datareader.readthedocs.io/en/latest/
+
+    # pull data into a dataframe
+
+    # fig = df.plot()
+    # return fig
+
+    
+
+
+if __name__ == "__main__":
+
+    fig = make_random_figure()
+    plt.show()
