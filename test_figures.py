a12a7b40eab1dccaf36b090462b5ab32d0be7878
Andrew Boyer <asboyer@gmail.com>
2020-08-04 21:59:08 -0400

    data plot function (1st)

--- test_figures.py
+++ test_figures.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import DataFrame 
 import numpy
 import matplotlib.pyplot as plt
 
@@ -11,12 +12,17 @@ def make_random_figure():
     pass
 
     # make some fake data or use numpy to get random data
+    Data = {'Unemployment_Rate' : [6.1,5.8,5.7,5.7,5.8,5.6,5.5,5.3,5.2,5.2],
+            'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
+    }
     # or get some real data from here https://pandas-datareader.readthedocs.io/en/latest/
 
     # pull data into a dataframe
-
+    df = DataFrame(Data,columns=['Unemployment_Rate','Stock_Index_Price'])
     # fig = df.plot()
+    fig = df.plot(x ='Unemployment_Rate', y='Stock_Index_Price', kind = 'scatter')
     # return fig
+    return fig 
 
     
 
