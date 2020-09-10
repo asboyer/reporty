010b096730a70c3e3e25bb4d6a03640129fa275e
Andrew Boyer <asboyer@gmail.com>
2020-09-10 15:11:10 -0400

    test

--- library_user.py
+++ library_user.py
@@ -4,7 +4,7 @@ from email_credentials import password, sender_email
 # end user owns this function
 from pandas import DataFrame
 import numpy as np
-
+from matplotlib import pyplot as plt
 import random 
 number = random.randint(1, 100000)
 
@@ -13,12 +13,18 @@ def make_random_figure():
     df = DataFrame(data, columns=['Goldfish Sales','Stock_Index_Price'])
     df['Stock_Index_Price'] += 10
     return df
-
+    
+def make_plot():
+    dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+    dev_y = [38496, 42000, 46752, 49320, 53200,
+         56000, 62316, 64928, 67317, 68748, 73752]
+    plot = plt.plot(dev_x, dev_y)
+    return plot
 rec_email = "deepkernel1@gmail.com"
 
 if __name__ == "__main__":
 
-    figure_list = [make_random_figure() for i in range(5)]
+    figure_list = [make_plot() for i in range(5)]
     title_list = ['a', 'b', 'c', 'd']
     caption_list = ['aa', 'bb', 'cc', 'dd']
     
