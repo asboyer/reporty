106fe5b151ef5b43b739549c5853f1b10cde5cbe
Benjamin Tengelsen <btengelsen@intelycare.com>
2020-10-14 17:06:40 -0400

    cleaning up some packaging things

--- build/lib/reporty/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from reporty import *
-
-
-from os import path
-templates_dir = path.join(path.dirname(__file__), 'templates')
---
+++ reporty/__init__.py
@@ -0,0 +1,4 @@
+from ._reporty import *
+from os import path
+
+templates_dir = path.join(path.dirname(__file__), 'templates')
