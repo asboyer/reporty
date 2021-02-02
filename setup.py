fa9c6864952546a1c97b42c675eb04b209f8bc54
boyer <asboyer@gmail.com>
2021-02-02 13:49:18 -0500

    modified version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 DISTNAME = 'reporty'
 
-VERSION = '0.1.9'
+VERSION = '0.1.91'
 
 DESCRIPTION = 'Reporty is a python library that contains useful functions for organizing and distributing visual data'
 
